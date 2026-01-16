cbfd8aeddbfdf64536121e5ce6ad3016f80c1e71
TPJM <jorshm.tabpar@educa.jcyl.es>
2026-01-16 09:28:50 +0100

    First commit

---
+++ miProyecto_ED/src/miProyecto_ED/PrimerProyecto_ED.java
@@ -0,0 +1,10 @@
+package miProyecto_ED;
+
+public class PrimerProyecto_ED {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ miProyecto_ED/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module miProyecto_ED {
+}
