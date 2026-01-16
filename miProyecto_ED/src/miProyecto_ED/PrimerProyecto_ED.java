b2a15e7de085a8300b30d964267570692edefe51
TPJM <jorshm.tabpar@educa.jcyl.es>
2026-01-16 09:44:54 +0100

    Hello world commit

--- miProyecto_ED/src/miProyecto_ED/PrimerProyecto_ED.java
+++ miProyecto_ED/src/miProyecto_ED/PrimerProyecto_ED.java
@@ -3,7 +3,7 @@ package miProyecto_ED;
 public class PrimerProyecto_ED {
 
 	public static void main(String[] args) {
-		System.out.println("Hola mundo");
+		System.out.println("Hello world");
 	}
 
 }
